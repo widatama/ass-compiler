519c6fb2526580dab907af676a556f7c0db54e1c
weizhenye <weizhenye@gmail.com>
2025-01-30 16:56:34 +0800

    feat: add `defaultInfo` option close #19

--- src/stringifier.js
+++ src/stringifier.js
@@ -1,7 +1,10 @@
 import { defaultOptions } from './utils.js';
 
 export function stringifyInfo(info) {
-  return Object.keys(info).map((key) => `${key}: ${info[key]}`).join('\n');
+  return Object.keys(info)
+    .filter((key) => info[key] !== null)
+    .map((key) => `${key}: ${info[key]}`)
+    .join('\n');
 }
 
 function pad00(n) {
