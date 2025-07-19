7bd94690d9711fc55a720106d2dec06d04e25595
Danu Widatama <widatama@gmail.com>
2025-07-19 22:38:08 +0700

    Update dependencies

--- vitest.config.js
+++ vitest.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     include: ['test/**/*.js'],
-    exclude: ['test/fixtures/*.js'],
+    exclude: ['test/fixtures/*.js', 'temp/*.js'],
     reporters: process.env.GITHUB_ACTIONS ? ['github-actions'] : ['tap-flat', 'dot'],
   },
 });
